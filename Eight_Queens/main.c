6fbe3f06325c1d40fc65a32ec8c37f07dd48f28a
Mert Ceylan <mert@Merts-MacBook-Pro.local>
2014-07-05 22:19:43 +0300

    Initial Commit

---
+++ Eight_Queens/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Eight_Queens
+//
+//  Created by Mert Ceylan on 7/5/14.
+//  Copyright (c) 2014 SL. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
